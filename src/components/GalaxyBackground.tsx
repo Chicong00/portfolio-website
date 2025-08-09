c421ae2d1901049e0a7112885aad9a1f5b5045d4
Cong Vo <vochicong0402@gmail.com>
2025-08-09 22:42:53 +0700

    install gsap

--- src/components/GalaxyBackground.tsx
+++ src/components/GalaxyBackground.tsx
@@ -1,9 +1,40 @@
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+
 export function GalaxyBackground() {
+  const particlesRef = useRef([]);
+
+  useEffect(() => {
+    particlesRef.current.forEach((particle, index) => {
+      gsap.to(particle, {
+        y: "random(-50, 50)",
+        x: "random(-50, 50)",
+        opacity: "random(0.5, 1)",
+        duration: 3,
+        repeat: -1,
+        yoyo: true,
+        ease: "power1.inOut",
+        delay: index * 0.5,
+      });
+    });
+  }, []);
+
   return (
     <div className="floating-blobs-bg">
       {/* Soft Floating Blobs with beige and green colors */}
       {[...Array(6)].map((_, i) => (
-        <div key={i} className="floating-blob" />
+        <div
+          key={i}
+          ref={(el) => (particlesRef.current[i] = el)}
+          className="floating-blob"
+          style={{
+            width: "50px",
+            height: "50px",
+            backgroundColor: i % 2 === 0 ? "#3B5E51" : "#F4D1A6",
+            borderRadius: "50%",
+            position: "absolute",
+          }}
+        />
       ))}
     </div>
   );
